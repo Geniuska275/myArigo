86e30ddcbce145e3030a8df9b71961901bfcb587
Geniuska275 <aigbojie2020@gmail.com>
2024-06-06 12:46:23 +0100

    adverts added

--- Screens/AdvertsScreen.jsx
+++ Screens/AdvertsScreen.jsx
@@ -1,19 +1,46 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View,ImageBackground } from 'react-native'
+import { Octicons } from '@expo/vector-icons';
 import React from 'react'
 import Header from '../Components/Header'
 import constants from "expo-constants";
 import Displays from '../Components/Displays';
 import { ScrollView } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
 const AdvertsScreen = () => {
-  return (
+  const [items, setItems] = React.useState([
+    { name: 'Business Wyze', img:"",image:"https://images.unsplash.com/photo-1717313860625-4d4311b5f9d3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" },
+    { name: 'imperatrice', img:"",image:"https://images.unsplash.com/photo-1716916959437-c904cdabd0f3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8" },
+    { name: 'Sqe holdings', img:"",image:"https://images.unsplash.com/photo-1717318104110-d8ef457d9c2e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D" },
+    { name: 'Swift Exchange', img:"",image:"https://images.unsplash.com/photo-1717328802573-a7cc71415e8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: 'Brains Technologies.', img:"",image:"https://plus.unsplash.com/premium_photo-1717563132740-6903bac2cf85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D" },
+    { name: 'EMERALD', img:"",image:"https://images.unsplash.com/photo-1717501218511-768944e2c325?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D" },
+ 
+    // Add more items as needed
+  ]);
+
+  return (  
     <SafeAreaView style={styles.container}>
       <Header/>
       <Text style={styles.text}>Adverts</Text>
       <ScrollView>
+      <FlatGrid
+      itemDimension={130}
+      data={items}
+      style={styles.gridView}
+      spacing={15}
+      renderItem={({ item }) => (
+        <ImageBackground source={{uri:item.image}} style={styles.backgroundImage}>
+        <View style={{position:"absolute",top:15,left:10,flexDirection:"row",borderRadius:"20px",backgroundColor:"white",padding:5,gap:10,alignItems:"center"}}>
+        <Octicons name="dot-fill" size={24} color="blue" />
+          <Text  style={{fontWeight:"bold",color:"black",width:100}}
+          numberOfLines={1}
+          >{item.name}</Text>
+        </View>
+      </ImageBackground>
+      )}
+    />
 
-      <Displays route={"Post"} name={"Posts"}/>
-      <Displays route={"Adverts"} name={"Adverts"}/>
-      <Displays/>
+      
       </ScrollView>
 
 
@@ -28,18 +55,46 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     marginTop: constants.statusBarHeight,
   },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', 
+    position:"relative",
+    width:160,
+    height:150,
+    borderRadius:15
+  },
   text:{
     textAlign:"center",
     shadowOffset: { width: 2, height: 2 },
     shadowColor: "black",
     shadowOpacity: 0.1,
-    elevation:1,
-    
-    paddingVertical:15,
+    elevation:1,   
+    paddingVertical:20,
     marginTop:-3,
     fontWeight:"bold",
-    color:"#243c56",
-    // marginBottom:10
-    textTransform:"uppercase"
-  }
+    color:"#a0a0a0",
+    textTransform:"capitalize",
+    backgroundColor:"#fafafa"
+  },
+  gridView: {
+    marginTop: 10,
+    flex: 1,
+    marginHorizontal:20
+  },
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 150,
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
+  },
 })
